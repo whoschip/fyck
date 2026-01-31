ca34119482cb4a43230642592b892f5962628310
remote chip <itsjustchipoverhere@proton.me>
2026-01-31 13:49:06 +0700

    no more tool creation

--- src/api_server/serve.py
+++ src/api_server/serve.py
@@ -2,7 +2,7 @@ from typing import Union
 import time
 import os
 from api_server.routes.completions import router as completion_router
-from api_server.routes.create_function import router as function_creation_router
+# from api_server.routes.create_function import router as function_creation_router
 
 from fastapi import FastAPI
 
@@ -10,4 +10,5 @@ app = FastAPI()
 
 
 app.include_router(completion_router)
-app.include_router(function_creation_router)
+# app.include_router(function_creation_router)
+# might not be good
